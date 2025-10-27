628a0fe4526eda5b3de2839e33fff8b4f609b0ed
RaffayA04 <raffaychattha@gmail.com>
2025-10-27 15:19:23 -0700

    chore: add iOS/Swift .gitignore

---
+++ JobFinder/JobFinder/JobFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinderApp.swift
+//  JobFinder
+//
+//  Created by Umair Ahmed on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct JobFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
